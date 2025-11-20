83d8da59e8d63259c3897cf5275eea524112bf30
Tinel25 <108215117+Tinel25@users.noreply.github.com>
2025-11-20 18:25:39 +0100

    Update get-users.js

--- netlify/functions/get-users.js
+++ netlify/functions/get-users.js
@@ -1,1 +1,30 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const DATA_FILE = path.join('/tmp', 'users-data.json');
+
+exports.handler = async (event) => {
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        const content = await fs.readFile(DATA_FILE, 'utf-8');
+        const data = JSON.parse(content);
+
+        return {
+            statusCode: 200,
+            headers,
+            body: JSON.stringify(data)
+        };
+    } catch (error) {
+        return {
+            statusCode: 200,
+            headers,
+            body: JSON.stringify({ users: {} })
+        };
+    }
+};
 
